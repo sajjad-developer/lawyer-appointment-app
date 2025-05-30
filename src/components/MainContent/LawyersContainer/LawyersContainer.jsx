03a9835c0e243977513e0cd939ab5a8a66ac4d8e
sajjad-developer <sajjadeco04@gmail.com>
2025-05-31 02:48:57 +0600

    chore(lawyers-container section): remove extra button from this section

--- src/components/MainContent/LawyersContainer/LawyersContainer.jsx
+++ src/components/MainContent/LawyersContainer/LawyersContainer.jsx
@@ -18,7 +18,7 @@ const LawyersContainer = ({ lawyers }) => {
     <>
       <div
         ref={refSection}
-        className="grid grid-cols-1 gap-2 mx-2 md:mx-10 mt-6 lg:mx-20 lg:grid-cols-2 lg:gap-6"
+        className="grid grid-cols-1 gap-2 mx-2 mt-6 md:mx-10 lg:mx-20 lg:grid-cols-2 lg:gap-6"
       >
         {displayLawyers.map((lawyer) => (
           <LawyerCard key={lawyer.id} lawyer={lawyer}></LawyerCard>
@@ -29,17 +29,6 @@ const LawyersContainer = ({ lawyers }) => {
           to="#_"
           className="inline-block relative justify-center text-lg text-center group"
         >
-          <button className="inline-block relative text-lg cursor-pointer group">
-            <span className="block overflow-hidden relative z-10 px-5 py-3 font-medium leading-tight text-gray-800 rounded-lg border-2 border-gray-900 transition-colors duration-300 ease-out group-hover:text-white">
-              <span className="absolute inset-0 px-5 py-3 w-full h-full bg-gray-50 rounded-lg"></span>
-              <span className="absolute left-0 -ml-2 w-48 h-48 bg-gray-900 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 group-hover:-rotate-180 ease"></span>
-              <span className="relative">Button Text</span>
-            </span>
-            <span
-              className="absolute right-0 bottom-0 -mr-1 -mb-1 w-full h-12 bg-gray-900 rounded-lg transition-all duration-200 ease-linear group-hover:mb-0 group-hover:mr-0"
-              data-rounded="rounded-lg"
-            ></span>
-          </button>
           <button
             onClick={() => {
               setShowAll((prv) => !prv);
