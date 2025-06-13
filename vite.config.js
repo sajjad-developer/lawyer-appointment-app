45b99e8562d11daf97bfea3041ec2f4ac8f4eca1
sajjad-developer <sajjadeco04@gmail.com>
2025-06-13 10:58:27 +0600

    chore: configure vite-plugin-purgecss in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import htmlPurge from "vite-plugin-purgecss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    htmlPurge({
+      content: [
+        "./index.html", // My main HTML file
+        "./src/**/*.js", // My JS file
+        "./src/**/*.jsx", // My React JSX components,
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
